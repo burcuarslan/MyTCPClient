03bc2222fa17bd7dba21d94b68df8b4b30e512f3
SalihSalurr <76595617+burcuarslan@users.noreply.github.com>
2021-07-15 01:09:31 +0300

    v1

---
+++ MyTCPClient/MyTCPClient/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyTCPClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(myTCPClientcs1.msg));
+            }
+            else
+            {
+                textBox4.Text=myTCPClientcs1.readData;
+            }
+         
+            //myTCPClientcs1.msg(textBox4.Text);
+            //textBox4.Text = myTCPClientcs1.incomingText;
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //myTCPClientcs1.clickConncet = true;
+            myTCPClientcs1.Start(true,e);
+            button1.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            myTCPClientcs1.IPtext = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            myTCPClientcs1.portText = textBox2.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //myTCPClientcs1.clickSend = true;
+            myTCPClientcs1.Send(true,textBox3.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+           // myTCPClientcs1.sendText = textBox3.Text;
+        }
+    }
+}
---
+++ MyTCPClient/MyTCPClient/MyTCPClientcs.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace MyTCPClient
+{
+    public partial class MyTCPClientcs : Component
+    {
+     
+        TcpClient client = new TcpClient();
+        NetworkStream networkStream = default(NetworkStream);
+
+        public string readData = null;
+        public string IPtext;
+        public string portText;
+        public string sendText;
+        public string incomingText;
+        public bool clickConncet;
+        public bool clickSend;
+        public MyTCPClientcs()
+        {
+            InitializeComponent();
+        }
+
+        public MyTCPClientcs(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+        public void Start(bool clickConncet, EventArgs e)
+        {
+            try
+            {
+                if (clickConncet == true)
+                {
+                    client.Connect(IPtext, Int32.Parse(portText));
+                    Thread thread = new Thread(getMessage);
+                    thread.Start();
+                }
+            }
+            catch(Exception ex)
+            {
+                
+            }
+            
+             
+           
+        }
+        public void getMessage()
+        {
+            string returnData;
+            while (true)
+            {
+                networkStream = client.GetStream();
+                var buffsize = client.ReceiveBufferSize;
+                byte[] inStream = new byte[buffsize];
+
+                networkStream.Read(inStream, 0, buffsize);
+                returnData = System.Text.Encoding.ASCII.GetString(inStream);
+                readData = returnData;
+                msg();
+                
+            }
+        }
+        public void msg() 
+        {
+
+        }
+        public void Send(bool clickSend,string texts)
+        {
+            if (clickSend==true)
+            {
+                byte[] outstream = Encoding.ASCII.GetBytes(texts);
+                
+                networkStream.Write(outstream, 0, outstream.Length);
+                networkStream.Flush();
+            }
+           
+        }
+    }
+}
